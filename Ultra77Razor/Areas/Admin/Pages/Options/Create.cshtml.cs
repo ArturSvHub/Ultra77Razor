cdda79f890732cd06abd4f833746fe876f4d9aba
Artur Sveshnikov <archey.net@live.ru>
2022-08-09 18:59:51 +0300

    0908

--- Ultra77Razor/Areas/Admin/Pages/Options/Create.cshtml.cs
+++ Ultra77Razor/Areas/Admin/Pages/Options/Create.cshtml.cs
@@ -20,17 +20,30 @@ namespace Ultra77Razor.Areas.Admin.Pages.Options
 		public ProductOption ProductOption { get; set; } = new();
 		[BindProperty]
 		public IEnumerable<SelectListItem>? ProductOptionSelectedList { get; set; }
+		[BindProperty]
+		public List<OptionDetail> OptionDetails { get; set; }= new();
 		public void OnGet()
         {
-			ProductOptionSelectedList = _context.Products?.Select(i => new SelectListItem
+			ProductOptionSelectedList =_context.Products?.Select(i => new SelectListItem
 			{
 				Text = i.Name,
 				Value = i.Id.ToString()
 			});
 		}
-		public void OnPost()
+		public async Task<ActionResult> OnPostAsync()
 		{
-			
+			await _context.AddAsync(ProductOption);
+			await _context.SaveChangesAsync();
+			if (OptionDetails.Count > 0)
+			{
+				foreach(var item in OptionDetails)
+				{
+					await _context.AddAsync(item);
+					await _context.SaveChangesAsync();
+				}
+			}
+
+			return RedirectToPage("Index");
 		}
     }
 }
--- Ultra77Razor/Areas/Admin/Pages/Options/Edit.cshtml.cs
+++ Ultra77Razor/Areas/Admin/Pages/Options/Edit.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using UpakDataAccessLibrary.DataContext;
+
+using UpakModelsLibrary.Models;
 
 namespace Ultra77Razor.Areas.Admin.Pages.Options
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly MssqlContext _context;
+
+        public EditModel(MssqlContext context)
         {
+            _context = context;
         }
-    }
+
+		[BindProperty]
+		public ProductOption Option { get; set; } = new();
+		[BindProperty]
+		public IEnumerable<SelectListItem>? ProductOptionSelectedList { get; set; }
+		[BindProperty]
+		public List<OptionDetail> OptionDetails { get; set; } = new();
+		public async Task<ActionResult> OnGet(int? id)
+		{
+			Option =await _context.ProductOptions.FindAsync(id);
+			if (Option != null)
+			{
+				; ProductOptionSelectedList = _context.Products?.Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				});
+				OptionDetails = await _context.OptionDetails.Include(o => o.ProductOption).Where(o => o.ProductOptionId == Option.Id).ToListAsync();
+			}
+				return Page();
+		}
+	}
 }
